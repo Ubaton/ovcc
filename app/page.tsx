ede50b9e6f0b83b91d08527378b4116be6022e81
3liantte <eliantte49@gmail.com>
2025-04-15 16:00:59 +0200

    landing page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { ResizableNavbar } from "./components/Navbar/Navbar";
+import WelcomeMessage from "./components/WelcomeMessage/WelcomeMessage";
+import { HeroParallaxDemo } from "./components/HeroParallex/HeroParallex";
 
 
 
@@ -7,6 +9,8 @@ export default function Home() {
   return (
     <div>
       <ResizableNavbar />
+      <WelcomeMessage/>
+      <HeroParallaxDemo/>
       <h1 className="flex justify-center items-center pt-40 text-4xl bg-amber-300">
         Hello OVC<sup>2</sup>
       </h1>
