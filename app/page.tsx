44bf70e78565aa17a3db969c157c07190c0178dd
3liantte <eliantte49@gmail.com>
2025-04-15 19:53:45 +0200

    footer update for responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -13,28 +13,32 @@ export default function Home() {
       <WelcomeMessage/>
       <HeroParallaxDemo/>
       <StickyScrollRevealDemo/>
-      <div>
-        <footer className="bg-white py-8 text-center text-sm text-gray-600 space-y-4">
-          <div className="flex flex-wrap justify-center gap-6 text-gray-700 font-medium">
-            <a href="#">About</a>
-            <a href="#">Blog</a>
-            <a href="#">Jobs</a>
-            <a href="#">Press</a>
-            <a href="#">Accessibility</a>
-            <a href="#">Partners</a>
-          </div>
+      
+      {/* Footer section */}
 
-          <div className="flex justify-center gap-6 text-gray-600 text-xl">
-            <a href="#"><i className="fab fa-facebook-f" /></a>
-            <a href="#"><i className="fab fa-instagram" /></a>
-            <a href="#"><i className="fab fa-x-twitter" /></a>
-            <a href="#"><i className="fab fa-github" /></a>
-            <a href="#"><i className="fab fa-youtube" /></a>
-          </div>
+        <footer className="bg-white py-8 px-4 text-center text-sm text-gray-600 space-y-6">
+        {/* Links */}
+        <div className="flex flex-wrap justify-center gap-x-6 gap-y-2 text-gray-700 font-medium text-sm">
+          <a href="#">About</a>
+          <a href="#">Blog</a>
+          <a href="#">Jobs</a>
+          <a href="#">Press</a>
+          <a href="#">Accessibility</a>
+          <a href="#">Partners</a>
+        </div>
 
-          <p>© 2024 OVC<sup>2</sup>, Inc. All rights reserved.</p>
-        </footer>
-      </div>
+        {/* Icons */}
+        <div className="flex justify-center gap-6 text-gray-600 text-xl">
+          <a href="#"><i className="fab fa-facebook-f" /></a>
+          <a href="#"><i className="fab fa-instagram" /></a>
+          <a href="#"><i className="fab fa-x-twitter" /></a>
+          <a href="#"><i className="fab fa-github" /></a>
+          <a href="#"><i className="fab fa-youtube" /></a>
+        </div>
+
+        {/* Copyright */}
+        <p className="text-xs text-gray-500">© 2024 Your Company, Inc. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
