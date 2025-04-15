9697cef0449c41ee6e231dd18f91cde82b63eef6
3liantte <eliantte49@gmail.com>
2025-04-15 19:50:46 +0200

    Footer added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { ResizableNavbar } from "./components/Navbar/Navbar";
 import WelcomeMessage from "./components/WelcomeMessage/WelcomeMessage";
 import { HeroParallaxDemo } from "./components/HeroParallex/HeroParallex";
+import { StickyScrollRevealDemo } from "./components/StickyRoll/StickyRoll";
 
 
 
@@ -11,9 +12,29 @@ export default function Home() {
       <ResizableNavbar />
       <WelcomeMessage/>
       <HeroParallaxDemo/>
-      <h1 className="flex justify-center items-center pt-40 text-4xl bg-amber-300">
-        Hello OVC<sup>2</sup>
-      </h1>
+      <StickyScrollRevealDemo/>
+      <div>
+        <footer className="bg-white py-8 text-center text-sm text-gray-600 space-y-4">
+          <div className="flex flex-wrap justify-center gap-6 text-gray-700 font-medium">
+            <a href="#">About</a>
+            <a href="#">Blog</a>
+            <a href="#">Jobs</a>
+            <a href="#">Press</a>
+            <a href="#">Accessibility</a>
+            <a href="#">Partners</a>
+          </div>
+
+          <div className="flex justify-center gap-6 text-gray-600 text-xl">
+            <a href="#"><i className="fab fa-facebook-f" /></a>
+            <a href="#"><i className="fab fa-instagram" /></a>
+            <a href="#"><i className="fab fa-x-twitter" /></a>
+            <a href="#"><i className="fab fa-github" /></a>
+            <a href="#"><i className="fab fa-youtube" /></a>
+          </div>
+
+          <p>© 2024 OVC<sup>2</sup>, Inc. All rights reserved.</p>
+        </footer>
+      </div>
     </div>
   );
 }
