0321cd94c66c9320714e48fd040fdfbc507022d3
3liantte <eliantte49@gmail.com>
2025-04-15 15:59:47 +0200

    HeroParallex component

---
+++ app/components/HeroParallex/HeroParallex.tsx
@@ -0,0 +1,102 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "../ui/hero-parallex";
+
+export function HeroParallaxDemo() {
+  return <HeroParallax products={products} />;
+}
+export const products = [
+  {
+    title: "Moonbeam",
+    link: "https://gomoonbeam.com", //Replace links with OVCC link to the home page
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "Cursor",
+    link: "https://cursor.so",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "Rogue",
+    link: "https://userogue.com",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+
+  {
+    title: "Editorially",
+    link: "https://editorially.org",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "Editrix AI",
+    link: "https://editrix.ai",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "Pixel Perfect",
+    link: "https://app.pixelperfect.quest",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+
+  {
+    title: "Algochurn",
+    link: "https://algochurn.com",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "Aceternity UI",
+    link: "https://ui.aceternity.com",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "https://tailwindmasterkit.com",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "SmartBridge",
+    link: "https://smartbridgetech.com",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "Renderwork Studio",
+    link: "https://renderwork.studio",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+
+  {
+    title: "Creme Digital",
+    link: "https://cremedigital.com",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "https://goldenbellsacademy.com",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "Invoker Labs",
+    link: "https://invoker.lol",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+  {
+    title: "E Free Invoice",
+    link: "https://efreeinvoice.com",
+    thumbnail:
+      "/assets/ovc2logo.jpg",
+  },
+];
