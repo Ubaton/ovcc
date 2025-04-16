2d356b38d2a86752e21b89265a5ca628f175b438
3liantte <eliantte49@gmail.com>
2025-04-16 10:41:07 +0200

    title change

--- app/components/ui/hero-parallex.tsx
+++ app/components/ui/hero-parallex.tsx
@@ -105,12 +105,13 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        The Ultimate <br /> development studio
+        Transforming Education, <br /> Empowering Generations
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-        We build beautiful products with the latest technologies and frameworks.
-        We are a team of passionate developers and designers that love to build
-        amazing products.
+        We&apos;re redefining what&apos;s possible in education—building innovative systems that
+        elevate every learner and empower every school. By uniting tradition with
+        cutting-edge technology, we&apos;re shaping smarter classrooms and brighter futures
+        for students across the globe.
       </p>
     </div>
   );
@@ -150,7 +151,7 @@ export const ProductCard = ({
           alt={product.title}
         />
       </Link>
-      <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none" />
+      <div className="absolute inset-0 h-full w-full opacity-0  pointer-events-none" />
       <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
         {product.title}
       </h2>
