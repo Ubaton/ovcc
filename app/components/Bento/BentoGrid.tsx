00676be39a4bfcc364e6ffa652fd43d260ef9a5b
3liantte <eliantte49@gmail.com>
2025-04-17 19:59:05 +0200

    bento update

--- app/components/Bento/BentoGrid.tsx
+++ app/components/Bento/BentoGrid.tsx
@@ -23,37 +23,96 @@ export function BentoGridDemo() {
     </BentoGrid>
   );
 }
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black" />
-);
 const items = [
   {
-    title: "The Dawn of Innovation",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
+    title: "Extra Tutoring (Virtual & Physical)",
+    description: "Daily subject-specific classes by qualififed tutors, available in-person or virtually.",
+    header: (
+      <div className="w-full h-40 md:h-full relative overflow-hidden rounded-xl">
+        <img
+          src="/assets/online.jpg"
+          alt="Tutoring session"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    ),
     className: "md:col-span-2",
     icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
+    title: "Exam Practice Tools",
+    description: "Accessible exam practice tools to help you prepare for your exams.",
+    header: (
+      <div className="w-full h-40 md:h-full relative overflow-hidden rounded-xl">
+        <img
+          src="/assets/learning.jpg"
+          alt="Tutoring session"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    ),
     className: "md:col-span-1",
     icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
+    title: "Career Simulation Tool",
+    description: "Interactive mobile tool that matches learners APS scores with career pathways and university requirements.",
+    header: (
+      <div className="w-full h-40 md:h-full relative overflow-hidden rounded-xl">
+        <img
+          src="/assets/simulate.jpg"
+          alt="Tutoring session"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    ),
     className: "md:col-span-1",
     icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Power of Communication",
+    title: "Parental Profile Access",
     description:
-      "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
-    className: "md:col-span-2",
+      "Dedicated space for parents to monitor academic performance, attendance, and progress in real time.",
+      header: (
+        <div className="w-full h-40 md:h-full relative overflow-hidden rounded-xl">
+          <img
+            src="/assets/service.jpg"
+            alt="Tutoring session"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      ),
+      className: "md:col-span-2",
     icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Administrative Support",
+    description: "Digital tools for streamlining student data and academic management, specially tailored for high schools and higher education platforms.",
+    header: (
+      <div className="w-full h-40 md:h-full relative overflow-hidden rounded-xl">
+        <img
+          src="/assets/support.jpg"
+          alt="Tutoring session"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    ),
+    className: "md:col-span-2",
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Skils Development & Training",
+    description: "Practical and career-aligned learning for skills that matter beyond the classroom.",
+    header: (
+      <div className="w-full h-40 md:h-full relative overflow-hidden rounded-xl">
+        <img
+          src="/assets/practice.jpg"
+          alt="Tutoring session"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    ),
+    className: "md:col-span-1",
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
   },
 ];
