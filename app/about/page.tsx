84d57a0fa8444de08b9e09b8ea9bbc21b42c4165
3liantte <eliantte49@gmail.com>
2025-04-17 13:41:20 +0200

    about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
   return (
-    <div>
-      Hello
+    <div className="min-h-screen flex justify-center items-center bg-white text-black">
+      <h1 className="text-9xl font-bold">About Us</h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
