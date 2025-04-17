3c208f86ae21b2a70a2d39250abc29229cdd7b3c
3liantte <eliantte49@gmail.com>
2025-04-17 13:44:47 +0200

    schools section

---
+++ app/for-schools/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-white text-black">
+      <h1 className="text-9xl font-bold">School</h1>
+    </div>
+  );
+};
+
+export default page;
