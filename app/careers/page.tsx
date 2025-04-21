9bfdac5b36f74fd7df2000779b763b80aaedd47d
3liantte <eliantte49@gmail.com>
2025-04-21 11:03:14 +0200

    font size cahnge

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const page = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-white text-black">
-      <h1 className="text-9xl font-bold">Coming soon...</h1>
+    <div className="min-h-screen flex justify-center items-center text-black relative py-20 px-6 md:px-20 overflow-hidden">
+      <h1 className="text-2xl font-bold">Coming soon...</h1>
     </div>
   );
 };
