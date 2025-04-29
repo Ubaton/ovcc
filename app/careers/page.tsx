43199635e1ee348a72bb6c09e2b45b572b39ac59
3liantte <eliantte49@gmail.com>
2025-04-29 14:40:18 +0200

    careers page update

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import BackButton from '../components/BackButton/BackButton';
 
 const page = () => {
   return (
     <div className="min-h-screen flex justify-center items-center text-black relative py-20 px-6 md:px-20 overflow-hidden">
-      <h1 className="text-2xl font-bold">Coming soon...</h1>
+      <BackButton/>
+      <h1 className="text-2xl font-light italic">Coming soon...</h1>
     </div>
   );
 };
